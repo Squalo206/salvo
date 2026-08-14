7a519da321156c8531211d33934d398d3fb7151b
Squalo206 <123331418+Squalo206@users.noreply.github.com>
2026-08-14 12:02:46 +0200

    Comment out San Salvo location in monitor.py

--- monitor.py
+++ monitor.py
@@ -10,7 +10,7 @@ from datetime import datetime, timedelta, timezone
 
 # === LOCALITÀ DA MONITORARE (nome, lat, lon) ===
 LOCATIONS = [
-    ("San Salvo", 42.050, 14.717),
+   # ("San Salvo", 42.050, 14.717),
     ("Isernia",   41.5931, 14.2326),
 ]
 
